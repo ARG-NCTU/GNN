f4ef6f40b5d999b067a3d4dc492752345a30e302
bawkbak <hsiehbawkbak@gmail.com>
2023-02-02 23:08:04 +0800

    Feat: First stable version of training loop

--- gnn/trainer.py
+++ gnn/trainer.py
@@ -9,7 +9,12 @@ import torch_geometric
 
 
 from envs.env import EnvManager
-# from ddpg import DDPG
+from ddpg import DDPG
+
+# debug
+from models.gnn import GNN_Actor, GNN_Critic
+from models.vgg import VGG
+# debug
 
 torch.backends.cudnn.benchmark=True
     
@@ -26,21 +31,25 @@ def parse_args():
     parser.add_argument('--optimizer', default='adam', help='optimizer to train with')
     parser.add_argument('--niter', type=int, default=300, help='number of epochs to train for')
 
-
+    parser.add_argument('--model', default='ddpg.pth')
     parser.add_argument('--latent_vector_dim', type=int, default=128, help='')
-    parser.add_argument('--state_vector_dim', type=int, default=4, help='')
+    parser.add_argument('--state_vector_dim', type=int, default=0, help='')
     parser.add_argument('--action_space_dim', type=int, default=2, help='')
     parser.add_argument('--lra', default=1e-3, type=float)
     parser.add_argument('--lrc', default=1e-3, type=float)
+    parser.add_argument('--lre', default=1e-3, type=float)
+    parser.add_argument('--lrg', default=1e-3, type=float)
     parser.add_argument('--batch_size', default=64, type=int, help='batch size')
     parser.add_argument('--tau', default=.005, type=float)
     parser.add_argument('--gamma', default=.99, type=float)
     parser.add_argument('--capacity', default=500000, type=int)
     parser.add_argument('--episode', type=int, default=2000, help='training epoch size') 
+    parser.add_argument('--warmup', default=10000, type=int) 
     parser.add_argument('--cuda', default=True, action='store_true')
+    parser.add_argument('--channel', type=int, default=2)
     # usv
     parser.add_argument('--usv_agent_num', default=3, type=int, help='number of usv in multi-agent')
-    parser.add_argument('--usv_max_v', default=1.5, type=float, help='')
+    parser.add_argument('--usv_max_v', default=1, type=float, help='')
     parser.add_argument('--usv_collision_radius', default=2.5, type=float, help='')
     parser.add_argument('--usv_sense_radius', default=8, type=float, help='')
     # uav
@@ -61,89 +70,141 @@ def parse_args():
     args = parser.parse_args()
     return args
 
-def train(args, env, agent):
+def train(args, env, agent, device):
+
+    env.initialize()
+
 
     total_steps = 0
     ewma_reward = 0
+    
     for episode in range(args.episode):
-        # total_reward = 0
-        # env.initialize()
-        # state = env.fetchState()
-        # for step in itertools.count(start=1):
-        #     if step < args.warmup:
-        #         action = env.randomAction()
-        #     else:
-        #         action = agent.select_action(state, noise=False)
+        total_reward = 0
+        enable_reset = 0
+        observation = env.reset()
+        for step in itertools.count(start=1):
+            for agent_index in range(args.usv_agent_num + args.uav_agent_num):
+                if total_steps < args.warmup:
+                    action_array = env.randomAction(agent_index)
+                    action = action_array[0]
+                else:
+                    action = agent.selectAction(observation["local_fov"][agent_index])
+                
+
+                
+                next_observation, reward, finished = env.step(agent_index, action)
+                # (fov, state, action, reward, next_fov, next_state, finished)
+
+                # print(type(observation["local_fov"][agent_index]))
+                # print(type(observation["pos"][agent_index]))
+                # print(type())
+                # print(type())
+                # print(type())
+                # print(type())
+                # print(type())
+                # print(type())
+
+                agent.append(
+                    observation["local_fov"][agent_index], 
+                    observation["pos"][agent_index], 
+                    action, 
+                    reward, 
+                    next_observation["local_fov"][agent_index], 
+                    next_observation["pos"][agent_index], 
+                    finished
+                )
+                if total_steps >= args.warmup:
+                    agent.update()
             
-        #     next_state, reward, done = env.step(action)
-        #     agent.append(state, action, reward, next_state, done)
-        #     if total_steps >= args.warmup:
-        #         agent.update()
-            
-        #     state = next_state
-        #     total_reward += reward
-        #     total_steps += 1
-        #     if done:
-        #         ewma_reward = 0.05 * total_reward + (1 - 0.05) * ewma_reward
-        #         writer.add_scalar('Train/Episode Reward', total_reward,
-        #                           total_steps)
-        #         writer.add_scalar('Train/Ewma Reward', ewma_reward,
-        #                           total_steps)
-        #         print(
-        #             'Step: {}\tEpisode: {}\tLength: {:3d}\tTotal reward: {:.2f}\tEwma reward: {:.2f}'
-        #             .format(total_steps, episode, t, total_reward,
-        #                     ewma_reward))
-        #         break
+                observation = next_observation
+                total_reward += reward
+                total_steps += 1
+
+                if finished:
+                    enable_reset = 1
+                    print(
+                        'Step: {}\tEpisode: {}\tLength: {:3d}\tTotal reward: {:.2f}'
+                        .format(total_steps, episode, step, total_reward))
+            if enable_reset:
+                break
 
         if episode % 200 == 0 and episode != 0:
-            print("Do the test in episode: ", episode)
-            # pass
+            # print("Do the test in episode: ", episode)
+            pass
             # test(args, env, agent)
+    return
 
+def debug_encoder(args, env, agent, device):
+    agent = agent.to(device)
 
+    env.initialize()
+    state = env.reset()
+    data = state["local_fov"][0]
+    print(type(data))
+    print(np.shape(data))
+    data = np.array(data).reshape(1, 18, 18, args.channel)
 
-    # # old version
-    # env.initialize()
-    # env.agentPerceptionUpdate(0)
-    # data = env.getAgentFOV(0)
+    train_input = torch.tensor(data).to(device)
+    train_input = train_input.permute(0,3,1,2)
+    train_input = train_input.float()
 
-    # # data = np.zeros((10,64,64,3))
-    
-    # print(type(data))
-    # print(np.shape(data))
-    # data = np.array(data).reshape(1, 18, 18, 3)
-    # # data = np.array(data).reshape(1, 64, 64, 3)
-    # train_input = torch.tensor(data).to(device)
-    # train_input = train_input.permute(0,3,1,2)
-    # train_input = train_input.float()
-
-
-    # h = module["encoder"](train_input)
-
-    # print(np.shape(h))
-
+    h = agent(train_input)
+    print(type(h))
+    print(h.size())
+    print(h.view(1,-1).size())
     return
 
 
 if __name__ == "__main__":
     arg = parse_args()
 
+    if arg.cuda:
+        assert torch.cuda.is_available(), 'CUDA is not available.'
+        device = 'cuda'
+    else:
+        device = 'cpu'
     
-    
-    env = EnvManager(arg)
-
-    
-    # agent = DDPG(arg)
-
-    
-    # train(arg, env, agent)
 
     # nns = generate_nn_instance(
     #     6, 32, 3, "relu"
     # )
     # gnn = ModGNNConv(nns["gnn"], aggr="add").jittable
-    
-    
-    env.initialize()
 
-    env.manualDisplay()
+
+    env = EnvManager(arg)
+
+    ### Debug
+
+    # agent = VGG(arg.latent_vector_dim, arg.channel)
+    # debug_encoder(arg, env, agent, device)
+
+    # agent = GNN_Critic(128, 0, 2)
+    # test =  torch.randn(16, 128, 1, 1)
+    # test2 = torch.randn(16, 2, 1, 1)
+
+    # re = agent(test.reshape(16, 128), test2.reshape(16, 2))
+    ### Debug
+
+
+    ### Train single agent
+
+
+
+    ### Train single agent
+    
+    
+    ### Train
+
+    agent = DDPG(arg)
+    train(arg, env, agent, device)
+    agent.save(arg.model, checkpoint=True)
+
+    ### Train
+
+    ### Manual
+
+    # env.initialize()
+    # unused = env.reset()
+    # env.manualDisplay()
+
+    ### Manual
